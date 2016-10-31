49436787723c78455e979f401c4d63c337bfe9dd
kkhattab <kareemkhattab925@gmail.com>
2016-10-30 23:06:22 -0700

    Base class for helpers

---
+++ hw3/views/helpers/Base.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace kareemkevin\hw3\Views\Helpers;
+
+class Base {
+
+	private $view;
+
+	public function __construct( $view ){
+
+		$this->view = $view;
+	}
+
+	public function render( $data ){
+		
+	}
+}
