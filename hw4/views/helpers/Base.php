3c3db7495f0c7c7aaab6efb151320abf404ccbbc
Kevin Hou <kevin.hou@sjsu.edu>
2016-11-07 13:40:41 -0800

    init files/structure (using hw3)

---
+++ hw4/views/helpers/Base.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace kareemkevin\hw4\Views\Helpers;
+
+class Base {
+
+	private $view;
+
+	public function __construct( $view ){
+
+		$this->view = $view;
+	}
+
+	public function render( $data ){
+		
+	}
+}
