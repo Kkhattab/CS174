fbc14a6058c654062efe8bbbc2ea5616854a37ad
Kevin Hou <kevin.hou@sjsu.edu>
2016-11-09 23:56:04 -0800

    added unstyled form fields

--- hw4/app/Views/Landing.php
+++ hw4/app/Views/Landing.php
@@ -8,6 +8,13 @@ class Landing extends Base {
 		
 		// title from contoller $data["site_title"]
 		$html .= '<h1>' .  $data["site_title"] .'</h1>';
+		$html .= '<h2>Share your data in charts!</h2>';
+		$html .= '<form>';
+		$html .= '<label for="chartTitle">Chart Title</label>';
+		$html .= '<input type="text" name="chartTitle" id="chartTitle">';
+		$html .= '<textarea></textarea>';
+		$html .= '<button type="submit">Share</button>';
+		$html .= '</form>';
 
 		$html .= $this->render_footer($data);
 
