dd23f087af6f8841811e1e4377264d21666608e5
Kevin Hou <kevin.hou@sjsu.edu>
2016-11-10 01:30:06 -0800

    finished client-side validation

---
+++ hw4/public/scripts/main.js
@@ -0,0 +1,29 @@
+function validateDataEntry(){
+	var textArea = document.getElementById("dataEntry").value;
+	var lines = textArea.split("\n");
+	var numOfLines = lines.length;
+	console.log(numOfLines);
+	if(numOfLines > 50) { // validates max 50 lines in textarea
+		alert("Too many (> 50 lines) lines! You have " + numOfLines + " number of lines.");
+		return;
+	}
+	var valid = false;
+	var error = "";
+	for(var i = 0; i < numOfLines; i++){
+		if (lines[i].length > 80) { // validates max 80 char per line
+			alert("Too many (> 80) characters on line " + i + ": " + lines[i] + ".");
+			return;
+		}
+		var line_arr = lines[i].split(",");
+		if (line_arr.length != 3) { // validates 3 values per line
+			alert("Invalid number of values on line " + i + ".");
+			return;
+		}
+		if (line_arr[0] === "") { // validates first value of each line
+			alert("First value cannot be empty/blank on line " + i + ".");
+			return;
+		}
+	}
+	document.getElementById("chartForm").setAttribute("method", "post"); // sets form to send as post
+	document.getElementById("chartForm").submit();
+}
