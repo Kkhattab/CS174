73143e3be6396d28dc48b5f2c409c8a409689177
kkhattab <kareemkhattab925@gmail.com>
2016-11-12 02:12:25 -0800

    added comment to run createdb script

--- hw4/CreateDB.php
+++ hw4/CreateDB.php
@@ -1,6 +1,6 @@
 <?php
 // Entry point
-// run from command line
+// run from command line or visit http://localhost/hw4/createDB.php
 require 'vendor/autoload.php';
 $creator = new Models\CreateDB();
 echo $creator->initDb();
