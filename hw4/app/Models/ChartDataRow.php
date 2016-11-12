caec508303c7dc423c888a022aff1cb1ddf251cc
kkhattab <kareemkhattab925@gmail.com>
2016-11-12 13:22:15 -0800

    added loading and saving data for db

--- hw4/app/Models/ChartDataRow.php
+++ hw4/app/Models/ChartDataRow.php
@@ -3,6 +3,43 @@ namespace Models;
 
 class ChartDataRow extends Base {
     
+     public function load_data($hash) {
+    
+        $hash = self::escape($hash);
+        $query = "SELECT * FROM chart_data WHERE chart_hash = '$hash' ORDER BY `order`";
+        $result = self::query($query);
+        
+        if (mysql_num_rows($result) == 0) {
+            return false;
+        }
+        
+        $title = null;
+        $data = array();
+        while ($row = mysql_fetch_assoc($result)) {
+            if ($title === null) {
+                $title = $row['title'];
+            }
+            $data[] = array($row['label'], $row['value1'], $row['value2']);
+        }
+        
+        return array("title" => $title, "data" => $data);
+    }
+
+    public function save($title, $data) {
+        $md5 = hash("md5", $title);
+        $title = self::escape($title);
+        $sql_rows = array();
+        foreach ($data as $row) {
+            list($label, $first, $second) = $row;
+            $label = self::escape($label);
+            $first = floatval($first);
+            $second = floatval($second);
+            $sql_rows []= "('$md5','$title','$label',$sin,$cos)";
+        }
+        $query = "INSERT INTO `chart_data` VALUES ".implode(",", $sql_rows).";";
+        return self::query($query);
+    }
+
     public function createTable() {
         // Create a table
         // Hash is the primary key
@@ -16,14 +53,19 @@ class ChartDataRow extends Base {
     }
     
     public function insertSampleData() {
+         $rows = array();
         // I want a sine and cosine for data points
         $rows = array();
-        $md5 = hash("md5", "sincos");
-        for ($x = 0; $x < 12; $x+=1.2) {
-            $sin = sin($x);
-            $cos = cos($x);
-            $rows []= "('$md5','$x',$sin,$cos)";
+        //md5 is the type of hasing, and the second string is the string that is getting hashed
+        $md5 = hash("md5", "Sine & Cosine");
+        for ($x = 0; $x < 10; $x++) {
+            $v = $x * 1.2;
+            $sin = sin($v);
+            $cos = cos($v);
+            $rows []= "('$md5','$x','Sine & Cosine','$v',$sin,$cos)";
         }
+        // implode joins strings 
+        //implode(",", array("1","2","3")) will return "1,2,3"
         $query = "INSERT INTO `chart_data` VALUES ".implode(",", $rows).";";
         return self::query($query);
     }
