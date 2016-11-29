a72c93fde8882115801af4716aca20ad2dfe8eed
kkhattab <kareemkhattab925@gmail.com>
2016-11-28 23:24:18 -0800

    fixed DBHOST const missing

--- hw5/app/Configs/Config.php
+++ hw5/app/Configs/Config.php
@@ -9,7 +9,7 @@ class Config {
 
     const BASE_URL="http://localhost/hw5/";
     const DBUSER = "root";
-    const DBHOST = "127.0.0.1"
+    const DBHOST = "127.0.0.1";
     const DBPASS = "";
     const DBNAME = "localdb";
 
