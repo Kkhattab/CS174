f06ec28c264a034862d2a3051a03818a2b09e467
kkhattab <kareemkhattab925@gmail.com>
2016-11-28 23:39:17 -0800

    Added constants to the Config class.

--- hw5/app/Configs/Config.php
+++ hw5/app/Configs/Config.php
@@ -6,12 +6,22 @@ namespace Configs;
  */
 
 class Config {
-
-    const BASE_URL="http://localhost/hw5/";
-    const DBUSER = "root";
-    const DBHOST = "127.0.0.1";
-    const DBPASS = "";
-    const DBNAME = "localdb";
-
+        
+        // Database configuration
+        const BASE_URL="http://localhost/hw5/";
+        const DBUSER = "root";
+        const DBHOST = "127.0.0.1"; 
+        const DBPASS = "";
+        const DBNAME = "localdb";
+        
+        // General app configuration
+        const WISH_PRICE = 0.25;
+        const WISH_PRICE_CURRENCY = "USD";
+        const WISH_PRICE_CURRENCY_SIGN = "$";
+        const MAX_FRIENDS_TO_MAIL = 10;
+        
+        // Stripe api credentials
+        const STRIPE_PUBLIC_KEY = "pk_test_iGdK2tJhQasi3K83IQ3fUz63";
+        const STRIPE_PRIVATE_KEY = "sk_test_7YRCAd1vHNzpQ7XCGv6cTeUT";
 }
-    
+  
