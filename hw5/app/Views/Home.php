393bcc54f93488008431c6115a60d9d3f516d871
kkhattab <kareemkhattab925@gmail.com>
2016-11-30 02:12:41 -0800

    added frame style and frame color

--- hw5/app/Views/Home.php
+++ hw5/app/Views/Home.php
@@ -15,20 +15,19 @@ class Home extends Base {
      */
     
     public function render($data) {
-       
         // load javascript from /public/script/home.js
         $this->addScript('https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js');
         $this->addScript('home.js');
         
         // render page
         $html = $this->render_header($data);
-        
         $html .= 
         '<div class="wrapper">
             <div style="text-align: center">
                 <h1>Throw a Coin in the Fountain</h1>
                 <p class="quote">...and send your kind wishes to the ones you love most</p>
             </div>
+            <h2>Customize your wish</h2>
             <div class="column">
                 <label for="wisher">Your name:</label><br>
                 <input type="text" placeholder="John Doe" name="wisher" id="input_wisher"><br>
@@ -38,17 +37,34 @@ class Home extends Base {
                     <option value="well1">Drawing</option>
                     <option value="well2">3D</option>
                     <option value="well3">3D Realistic</option>
-                </select>
+                </select><br>
+                
+                <label for="border-style">Select picture frame style:</label><br>
+                <select name="border-style" id="input_border-style" onchange="javascript:changeWellBorder()">
+                    <option value="none">None</option>
+                    <option value="solid">Solid</option>
+                    <option value="dashed">Dashed</option>
+                </select><br>
+                
+                <label for="border-color">Select picture frame color:</label><br>
+                <select name="border-color" id="input_border-color" onchange="javascript:changeWellBorder()">
+                    <option value="black">Black</option>
+                    <option value="red">Red</option>
+                    <option value="green">Green</option>
+                    <option value="purple">Purple</option>
+                    <option value="orange">Orange</option>
+                </select><br>
             </div>
             <div class="column">
-                <img src="./public/images/well1.jpg" id="well-image">
+                <div id="well-container">
+                    <img src="./public/images/well1.jpg" id="well-image">
+                </div>
             </div>
             <div class="clear"></div>
+            <hr>
+            <h2>Throw that coin</h2>
         </div>';
-        
         $html .= $this->render_footer($data);
-        
         return $html;
     }
-    
 }
--- hw5/public/scripts/home.js
+++ hw5/public/scripts/home.js
@@ -5,3 +5,23 @@ function changeWellImage() {
     $("#well-image").attr("src", "./public/images/" + image_name + ".jpg");
     
 }
+
+function changeWellBorder() {
+    
+    var border_style = $("#input_border-style").val();
+    var border_color = $("#input_border-color").val();
+    var border, width;
+    
+    if (border_style == 'none') {
+        border = 'none';
+        width = '265px';
+    } else {
+        border = '3px ' + border_style + ' ' + border_color;
+        width = '259px'; // 265 - 2 * 3
+    }
+    
+    $("#well-container")
+            .css('border', border) // set border with jquery
+            .css('width', width); // adjust width
+    
+}
